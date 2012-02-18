efaf8bdd70e6a2f24433b8d6f16b57f4044c7de3
SirTyler <sirtyler132@gmail.com>
2012-02-17 22:34:11 -0500

    Edit
    Signed-off-by: SirTyler <sirtyler132@gmail.com>

---
+++ Ava/src/main/java/org/avateam/ava/AvaLogic.java
@@ -0,0 +1,16 @@
+package org.avateam.ava;
+
+import com.topcat.npclib.entity.HumanNPC;
+
+public class AvaLogic {
+	@SuppressWarnings("unused")
+	private Main plugin;
+	
+	public AvaLogic(Main instance) {
+		plugin = instance;
+	}
+
+	public void ChopWood(HumanNPC npc) {
+		//Wood Code
+	}
+}
